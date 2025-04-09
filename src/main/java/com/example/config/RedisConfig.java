b6000f7c760a77ed54bc10713f6115bd78d11640
npu2020303144 <1658596069@qq.com>
2025-04-09 17:19:36 +0800

    拆分了RedisUtil（工具类）和RedisConfig类（配置类），解决循环依赖错误

--- src/main/java/com/example/config/RedisConfig.java
+++ src/main/java/com/example/config/RedisConfig.java
@@ -9,14 +9,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    //编写我们自己的RedisTemplate(这是固定模板)
 
     @Bean
     /*
     *   spring-data-redis 的 RedisTemplate<K, V>模板类 在操作redis时默认使用JdkSerializationRedisSerializer
         来进行序列化。spring操作redis是在jedis客户端基础上进行的，而jedis客户端与redis交互的时候协议中定义是用byte类型交互，
         看到spring-data-redis中RedisTemplate<K, V>在操作的时候k，v是泛型对象，而不是byte[]类型的，
-        这样导致的一个问题就是，如果不对RedisTemplate进行设置，spring会默认采用
+        这样导致，如果不对RedisTemplate进行设置，spring会默认采用
         defaultSerializer = new JdkSerializationRedisSerializer();
         这个方法来对key、value进行序列化操作，JdkSerializationRedisSerializer它使用的编码是ISO-8859-1
     * */
@@ -24,10 +23,14 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         //设置key序列化
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
+        // 设置 value 序列化
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
         template.afterPropertiesSet();
         return template;
     }
--- src/main/java/com/example/utils/JwtTokenUtil.java
+++ src/main/java/com/example/utils/JwtTokenUtil.java
@@ -3,7 +3,6 @@ package com.example.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
